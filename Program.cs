33ea35f18f89ba523a862004bdc57fe74a319f51
Trey Walker <Treywalk33@gmail.com>
2024-11-08 16:42:22 -0700

    step 2 and start of choices

--- Program.cs
+++ Program.cs
@@ -20,7 +20,26 @@ namespace ProjectEuler
 
                 int choice = int.Parse(Console.ReadLine());
 
-            
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Result: " + MultiplesOf3And5(1000));
+                        break;
+                    case 2:
+                        Console.WriteLine("Result: " + EvenFibonacciNumbers(4000000));
+                        break;
+                    case 3:
+                        Console.WriteLine("Result: " + LargestPrimeFactor(600851475143));
+                        break;
+                    case 4:
+                        Console.WriteLine("Result: " + LargestPalindromeProduct());
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
             }
         }
     }
