c539e5fb31ec61c7a441cd9da028f1ef1d78c5d7
Trey Walker <Treywalk33@gmail.com>
2024-11-08 16:24:02 -0700

    step 1 menu

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Trey Walker, 12-8-24, Euler
+
+using System; 
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project Euler Solver");
+
+            while (true)
+            {
+                Console.WriteLine("Choose a problem:");
+                Console.WriteLine("1. Multiples of 3 and 5");
+                Console.WriteLine("2. Even Fibonacci Numbers");
+                Console.WriteLine("3. Largest Prime Factor");
+                Console.WriteLine("4. Largest Palindrome Product");
+                Console.WriteLine("5. Exit");
+
+                int choice = int.Parse(Console.ReadLine());
+
+            
+            }
+        }
+    }
+}
